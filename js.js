11a0b1416cdf60bb3a5cc7b1e1a8604b83de225a
Giordano Bruno <giordanobrunotj@hotmail.com>
2023-11-24 16:53:09 +0000

    calc

--- js.js
+++ js.js
@@ -2,15 +2,15 @@ function calculation(){
 
     var stock = document.getElementById("stock").value;
     var buydate = document.getElementById("buydate").value;
-    var buyprice = document.getElementById("buyprice").value;
-    var buyconversion = document.getElementById("buyconversion").value;
-    var buyfees = document.getElementById("buyfees").value;
-    var sellprice = document.getElementById("sellprice").value;
-    var sellconversion = document.getElementById("sellconversion").value;
-    var sellfees = document.getElementById("sellfees").value;
+    var buyprice = parseFloat(document.getElementById("buyprice").value);
+    var buyconversion = parseFloat(document.getElementById("buyconversion").value);
+    var buyfees = parseFloat(document.getElementById("buyfees").value);
+    var sellprice = parseFloat(document.getElementById("sellprice").value);
+    var sellconversion = parseFloat(document.getElementById("sellconversion").value);
+    var sellfees = parseFloat(document.getElementById("sellfees").value);
     var selldate = document.getElementById("selldate").value;
     var buyconverted = (buyprice + buyfees)*buyconversion;
     var sellcoverted = (sellprice + sellfees)*sellconversion;
-    console.log(buyfees);
+    console.log(buyconversion);
     document.getElementById("profit").innerHTML = sellcoverted - buyconverted;
 }
