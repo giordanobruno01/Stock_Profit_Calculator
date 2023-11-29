964e99a6988e56cb5876c447b4f142617e15a574
Giordano Bruno <giordanobrunotj@hotmail.com>
2023-11-29 18:25:44 +0000

    calc3

--- js.js
+++ js.js
@@ -1,28 +1,70 @@
-function calculation(){
+function calculation() {
+  var stock = document.getElementById("stock").value;
+  var buydate = new Date(document.getElementById("buydate").value);
+  var buyprice = parseFloat(document.getElementById("buyprice").value);
+  var buyconversion = parseFloat(
+    document.getElementById("buyconversion").value === ""
+      ? 1
+      : document.getElementById("buyconversion").value
+  );
+  var buyfees = parseFloat(
+    document.getElementById("buyfees").value === ""
+      ? 0
+      : document.getElementById("buyfees").value
+  );
+  var sellprice = parseFloat(document.getElementById("sellprice").value);
+  var sellconversion = parseFloat(
+    document.getElementById("sellconversion").value === ""
+      ? 1
+      : document.getElementById("sellconversion").value
+  );
+  var sellfees = parseFloat(
+    document.getElementById("sellfees").value === ""
+      ? 0
+      : document.getElementById("sellfees").value
+  );
+  var selldate = new Date(document.getElementById("selldate").value);
 
-    var stock = document.getElementById("stock").value;
-    var buydate =  new Date(document.getElementById("buydate").value) ;
-    var buyprice = parseFloat(document.getElementById("buyprice").value);
-    var buyconversion = parseFloat(document.getElementById("buyconversion").value === "" ? 1 :document.getElementById("buyconversion").value);
-    var buyfees = parseFloat(document.getElementById("buyfees").value === "" ? 0 :document.getElementById("buyfees").value);
-    var sellprice = parseFloat(document.getElementById("sellprice").value);
-    var sellconversion = parseFloat(document.getElementById("sellconversion").value === "" ? 1 :document.getElementById("sellconversion").value);
-    var sellfees = parseFloat(document.getElementById("sellfees").value === "" ? 0 :document.getElementById("sellfees").value);
-    var selldate = new Date(document.getElementById("selldate").value) ;
-    
-    if(isNaN(buyprice) || isNaN(sellprice)){
-       
-        alert("Prices are mandatory")
-        return 
-    }
-   
-    var buyconverted = (buyprice + buyfees)/buyconversion;
-    var sellcoverted = (sellprice - sellfees)/sellconversion;
+  if (isNaN(buyprice) || isNaN(sellprice)) {
+    alert("Prices are mandatory");
+    return;
+  }
 
-    var netresult = sellcoverted - buyconverted
-    document.getElementById("profit").innerHTML = netresult.toFixed(2) ;
-    var timediff = selldate - buydate
-    document.getElementById("days").innerHTML = Math.ceil(timediff/ (1000 * 3600 * 24));
-     
-    document.getElementById("percentage").innerHTML = (netresult*100/(buyconverted+sellfees)).toFixed(2) 
+  var buyconverted = (buyprice + buyfees) / buyconversion;
+  var sellcoverted = (sellprice - sellfees) / sellconversion;
+
+  var netresult = sellcoverted - buyconverted;
+  document.getElementById("profit").innerHTML = netresult.toFixed(2);
+  var timediff = selldate - buydate;
+  document.getElementById("days").innerHTML = Math.ceil(
+    timediff / (1000 * 3600 * 24)
+  );
+
+  document.getElementById("percentage").innerHTML = (
+    (netresult * 100) /
+    (buyconverted + sellfees)
+  ).toFixed(2);
 }
+function displaydollar(){
+    document.getElementById("dollar").style.display = "block";
+    document.getElementById("othercurr").style.display = "none";
+}
+function displayother() {
+  document.getElementById("dollar").style.display = "none";
+  document.getElementById("othercurr").style.display = "block";
+}
+//   $(document).ready(function () {
+//     console.log("hello");
+//     $("#optdollar").click(function () {
+//       console.log("hello");
+//       $("#dollar").toggle();
+//       $("#othercurr").hide();
+//     });
+//   });
+//   $(document).ready(function () {
+//     $("#optother").click(function () {
+//       $("#othercurr").show();
+//       $("#dollar").hide();
+//     });
+//   });
+
